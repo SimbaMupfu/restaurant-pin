4151823bee2712110d5f522bb703769b44504903
simba_mupfu <scmupfururirwa@gmail.com>
2023-10-02 08:01:37 +0200

    Inject the managed object context to access it easily and create the data model

--- RestaurantPin/RestaurantPinApp.swift
+++ RestaurantPin/RestaurantPinApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct RestaurantPinApp: App {
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             RestaurantListView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
     
